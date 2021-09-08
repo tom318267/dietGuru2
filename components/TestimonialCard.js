f76590cd29dd5127d8b5ce43403f6fd2007fedb3
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-09-08 12:57:02 -0400

    adjust testimonial card

--- components/TestimonialCard.js
+++ components/TestimonialCard.js
@@ -13,11 +13,11 @@ const TestimonialCard = ({ image, name, title }) => {
       className="TestimonialCard bg-darkGreen py-20 px-8 h-auto mx-10 rounded-xl mb-10"
     >
       <div className="flex flex-col sm:flex-row items-center">
-        <div className="pr-5 mb-4">
+        <div className="sm:mr-5 mb-4">
           <Image layout="fixed" src={image} objectFit="cover" />
         </div>
 
-        <div className="text-white">
+        <div className="text-white text-center sm:text-left">
           <h1 className="text-2xl sm:text-3xl md:text-4xl mb-4">"{title}"</h1>
           <p className="mb-3 text-sm sm:text-md md:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
